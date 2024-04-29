df5dbbaa3043624b049b8e140009bb12b839b929
Gabriela Vega Lobatos <104038436+gvega2608@users.noreply.github.com>
2024-04-29 13:52:57 -0400

    Update FrameDisplay.java

--- FrameDisplay.java
+++ FrameDisplay.java
@@ -1,11 +1,6 @@
 import javax.swing.*;
 import java.util.ArrayList;
 
-/**
- * Defines a frame to which a panel with drawings will be added.
- *
- * @author add here name, Panther ID, and class section of all authors
- */
 public class FrameDisplay extends JFrame
 {
     int WIDTH = 600;
